9d6e8f9f5cbef8dba0041dc57a3df5030816b2fa
Marek Gogoľ <info@marekgogol.sk>
2021-04-15 21:40:04 +0200

    added blocks render method

--- src/Contracts/Blocks/BlocksBuilder.php
+++ src/Contracts/Blocks/BlocksBuilder.php
@@ -29,19 +29,24 @@ class BlocksBuilder
         return config('admin.gutenberg_wrapper_class');
     }
 
+    public function renderBlocks()
+    {
+        $blocks = (new \WP_Block_Parser)->parse($this->html);
+
+        $rendered = [];
+
+        foreach ($blocks as $blockData) {
+            $rendered[] = (new \WP_Block($blockData))->render();
+        }
+
+        return $rendered;
+    }
+
     /**
      * Renders any blocks in the HTML (recursively)
      */
     public function render()
     {
-        $blocks = (new \WP_Block_Parser)->parse($this->html);
-
-        $content = [];
-
-        foreach ($blocks as $blockData) {
-            $content[] = (new \WP_Block($blockData))->render();
-        }
-
-        return '<div class="'.$this->getWrapperClass().'">'.implode('', $content).'</div>';
+        return '<div class="'.$this->getWrapperClass().'">'.implode('', $this->renderBlocks()).'</div>';
     }
 }
